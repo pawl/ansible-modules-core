1aecee9b05a76b5c6e2fadbcf39dbaec7483c7c8
Paul Brown <paul90brown@gmail.com>
2020-10-10 19:24:58 -0500

    update django_manage migrate docs to include database param

--- web_infrastructure/django_manage.py
+++ web_infrastructure/django_manage.py
@@ -63,7 +63,7 @@ options:
     required: false
   database:
     description:
-      - The database to target. Used by the 'createcachetable', 'flush', 'loaddata', and 'syncdb' commands.
+      - The database to target. Used by the 'createcachetable', 'flush', 'loaddata', 'syncdb', and 'migrate' commands.
     required: false
   failfast:
     description:
